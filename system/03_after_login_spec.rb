9bb2d36a5fb648eb5994cc8e826796c7f53be5b5
EC2 Default User <leandro83g@icloud.com>
2021-09-29 05:40:31 +0000

    feat: add tests for header links because those are needed

--- system/03_after_login_spec.rb
+++ system/03_after_login_spec.rb
@@ -85,6 +85,28 @@ describe '[STEP3] ユーザログイン後のテスト' do
   # 「トップページ」「ユーザーの一覧画面」「投稿された本一覧画面」「ログアウト処理」にリダイレクトしている
   #***********************************************************************************************************
   describe 'ヘッダーのテスト: ログインしている場合' do
+    context '表示内容の確認' do
+      it 'タイトルが表示される' do
+        expect(page).to have_content 'Bookers'
+      end
+      it 'Homeリンクが表示される: 左上から1番目のリンクが「Home」である' do
+        home_link = find_all('a')[1].native.inner_text
+        expect(home_link).to match(/home/i)
+      end
+      it 'Usersリンクが表示される: 左上から2番目のリンクが「Users」である' do
+        users_link = find_all('a')[2].native.inner_text
+        expect(users_link).to match(/users/i)
+      end
+      it 'Booksリンクが表示される: 左上から3番目のリンクが「Books」である' do
+        books_link = find_all('a')[3].native.inner_text
+        expect(books_link).to match(/books/i)
+      end
+      it 'log outリンクが表示される: 左上から4番目のリンクが「logout」である' do
+        logout_link = find_all('a')[4].native.inner_text
+        expect(logout_link).to match(/logout/i)
+      end
+    end
+    
     context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済みになります。' do
       subject { current_path }
 
@@ -107,8 +129,26 @@ describe '[STEP3] ユーザログイン後のテスト' do
         is_expected.to eq '/books'
       end
     end
-
-
+    
+    describe 'ユーザログアウトのテスト' do
+      # let(:user) { create(:user) }
+  
+      before do
+        # visit new_user_session_path
+        # fill_in 'user[name]', with: user.name
+        # fill_in 'user[password]', with: user.password
+        # click_button 'Log in'
+        logout_link = find_all('a')[4].native.inner_text
+        logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link logout_link
+      end
+  
+      context 'ログアウト機能のテスト' do
+        it 'ログアウト後のリダイレクト先が、トップになっている' do
+          expect(current_path).to eq '/'
+        end
+      end
+    end
   end
   #***********************************
   #user/index
